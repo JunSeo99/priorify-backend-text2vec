e38126e5e78b9970fb8ca20e900af61dc8a7eb5d
jihoon <assj7223@naver.com>
2025-05-26 17:45:06 +0900

    NER 모델 변경: yongsun-yoon/mdeberta-v3-base-open-ner

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import faiss
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
-ner_tokenizer = AutoTokenizer.from_pretrained("joon09/kor-naver-ner-name")
-ner_model     = AutoModelForTokenClassification.from_pretrained("joon09/kor-naver-ner-name")
+ner_tokenizer = AutoTokenizer.from_pretrained("yongsun-yoon/mdeberta-v3-base-open-ner")
+ner_model     = AutoModelForTokenClassification.from_pretrained("yongsun-yoon/mdeberta-v3-base-open-ner")
 ner_pipeline  = pipeline(
     "ner",
     model=ner_model,
