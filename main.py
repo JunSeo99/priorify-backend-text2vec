27d15e0aa5a21c858c8f65adb339c7494e9010c8
jihoon <assj7223@naver.com>
2025-05-26 18:33:19 +0900

    NER 모델 복귀: Leo97/KoELECTRA-small-v3-modu-ner - 최종 선택

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import faiss
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
-ner_tokenizer = AutoTokenizer.from_pretrained("SungJoo/medical-ner-koelectra")
-ner_model     = AutoModelForTokenClassification.from_pretrained("SungJoo/medical-ner-koelectra")
+ner_tokenizer = AutoTokenizer.from_pretrained("Leo97/KoELECTRA-small-v3-modu-ner")
+ner_model     = AutoModelForTokenClassification.from_pretrained("Leo97/KoELECTRA-small-v3-modu-ner")
 ner_pipeline  = pipeline(
     "ner",
     model=ner_model,
